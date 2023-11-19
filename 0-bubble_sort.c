632d5e775dc9e19cf66b1f1dbaa55efc1f23559b
Wandile Mawelela <wandile1997@gmail.com>
2023-11-19 09:54:45 +0200

    [FIX] Edge cases

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,10 +13,17 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp;
+	int tmp, swapped;
+
+	if (array == NULL || size == 0)
+	{
+		printf("Array is empty or NULL.\n");
+		return;
+	}
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -24,8 +31,15 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+				swapped = 1;
 				print_array(array, size);
 			}
+		}
+
+		if (swapped == 0)
+		{
+			printf("Array is already sorted.\n");
+			break;
 		}
 	}
 }
