e973d53969fb330b011d0a22a07c154e1d666def
Wandile Mawelela <wandile1997@gmail.com>
2023-11-21 19:02:01 +0200

    [ADD] Completed task 2: Write a function that sorts an array of integers in ascending order using the Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: Array to be sorted
+ * @size: Sixe of the array
+ *
+ * Return: Nothing
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k, tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		k = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[k])
+			{
+				k = j;
+				continue;
+			}
+		}
+
+		if (k != i)
+		{
+			tmp = array[i];
+			array[i] = array[k];
+			array[k] = tmp;
+			print_array(array, size);
+		}
+	}
+}
